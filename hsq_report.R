ac8936bfcda860a63f64761addd42f8ae9ab7daf
John Stanton-Geddes <john.stantongeddes.research@gmail.com>
2013-06-03 15:46:17 -0500

    Update hsq_report.r to include sample in the output title

--- hsq_report.R
+++ hsq_report.R
@@ -10,4 +10,4 @@ iter <- args[3]
 hsq <- read.table(paste("reml_", trait, "_", sample, "_", iter, ".hsq", sep=""), sep="\t", header=TRUE, nrows=4)
 
 # write results of 'V(G)/Vp' to file
-cat(trait, "\t", sample, "\t", iter, "\t", hsq[4,2], "\t", hsq[4,3], "\n", file=paste("../H2gcta_", trait, ".txt", sep=""), append=TRUE)
+cat(trait, "\t", sample, "\t", iter, "\t", hsq[4,2], "\t", hsq[4,3], "\n", file=paste("../H2gcta_", sample,"_", trait, ".txt", sep=""), append=TRUE)
